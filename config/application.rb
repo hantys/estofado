200560cc7572a8a6d5df4136589e6e71df3c0e93
Pedro Fausto <pedro.fausto@hotmail.com>
2019-05-27 03:08:32 -0300

    application config

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,44 @@ module Estofado
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = 'America/Fortaleza'
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
+    config.i18n.load_path += Dir["#{Rails.root}/config/locales/**/*.{rb,yml}"]
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = ['pt-BR']
+
+    config.action_controller.include_all_helpers = true
+
+    config.action_mailer.default_url_options = if Rails.env.development?
+                                                 { host: 'http://127.0.0.1', port: 3000 }
+                                               else
+                                                 { host: 'https://nacionalservicos.com.br' }
+                                               end
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address: "smtp.nacionalservicos.com.br",
+      port: 587,
+      domain: 'nacionalservicos.com.br',
+      user_name: 'sistema@nacionalservicos.com.br',
+      password: '3rf454y65her',
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir["#{config.root}/app/models/concerns/**"]
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       routing_specs: false,
+                       view_specs: true,
+                       helper_specs: true,
+                       controller_specs: false
+    end
   end
 end
